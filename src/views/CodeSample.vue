f3a3df86c6dba0e9240540c5c89cc1fc1aa3a8ad
min.kim <yois56@gmail.com>
2021-04-02 18:33:13 +0900

    Swiper test

--- src/views/CodeSample.vue
+++ src/views/CodeSample.vue
@@ -27,17 +27,63 @@
     </div>
 
 
+
+
+    <swiper class="swiper1" :options="swiperOptions" ref="mySwiper">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+      <swiper-slide>Slide 6</swiper-slide>
+      <swiper-slide>Slide 7</swiper-slide>
+      <swiper-slide>Slide 8</swiper-slide>
+      <swiper-slide>Slide 9</swiper-slide>
+      <swiper-slide>Slide 10</swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+
 		
 	</pages-layout>
 </template>
 
 <script>
+  // swiper
+  import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+  import 'swiper/css/swiper.css'
+
 	export default {
 		name: 'CodeSample',
+    //title: '타이틀',
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
 		data () {
 			return {
 				uiActiveTabIdx: 0,
         uiIsActiveFilter: false,
+        swiperOptions: {
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          },
+          speed: 500,
+          loop: true,
+          loopFillGroupWithBlank: true,
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          // autoplay: {
+          //   delay: 3000,
+          //   disableOnInteraction: false
+          // }
+        }
 			}
 		},
 
@@ -56,6 +102,7 @@
         
        
       },
+
 
 			// --------
       // 진입
@@ -113,6 +160,9 @@
     created () {
       
     },
+    mounted() {
+      console.log('Swiper instances:', this.$refs.mySwiper)
+    }
 		
 	};
 </script>
